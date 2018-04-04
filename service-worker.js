2b3950f9d25b502979218cea57303ce0f56de5cb
zontaya <zontaya@gmail.com>
2018-04-04 13:40:54 +0700

    -

--- service-worker.js
+++ service-worker.js
@@ -25,6 +25,19 @@ self.addEventListener('install', function(e) {
   );
 });
 
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
