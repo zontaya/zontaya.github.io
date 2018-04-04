3dae1629b3f5ff280b35aa549198a027bad64264
zontaya <zontaya@gmail.com>
2018-04-04 15:59:59 +0700

    -

---
+++ service-worker2.js
@@ -0,0 +1,49 @@
+var cacheName = "weatherPWA";
+var filesToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/styles.css",
+  "/mdl/material.min.js",
+  "/images/icons/icon-128x128.png",
+  "/images/icons/icon-144x144.png",
+  "/images/icons/icon-152x152.png",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-256x256.png",
+  "/images/icons/icon-512x512.png"
+];
+
+
+//This is the "Offline page" service worker
+
+//Install stage sets up the offline page in the cahche and opens a new cache
+self.addEventListener('install', function(event) {
+  var offlinePage = new Request('offline.html');
+  event.waitUntil(
+  fetch(offlinePage).then(function(response) {
+    return caches.open(cacheName).then(function(cache) {
+      console.log('[PWA Builder] Cached offline page during Install'+ response.url);
+      return cache.put(offlinePage, response);
+    });
+  }));
+});
+
+//If any fetch fails, it will show the offline page.
+//Maybe this should be limited to HTML documents?
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+        console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
+        return caches.open(cacheName).then(function(cache) {
+          return cache.match('offline.html');
+      });
+    }));
+});
+
+//This is a event that can be fired from your page to tell the SW to update the offline page
+self.addEventListener('refreshOffline', function(response) {
+  return caches.open(cacheName).then(function(cache) {
+    console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
+    return cache.put(offlinePage, response);
+  });
+});
