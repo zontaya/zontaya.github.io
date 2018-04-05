8b054b45d89df85cff1d6a22dcdbaa76f5274354
zontaya <zontaya@gmail.com>
2018-04-05 15:08:56 +0700

    -

--- service-worker.js
+++ service-worker.js
@@ -28,12 +28,17 @@ self.addEventListener("install", function(e) {
       })
   );
 });
+
 self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.open(cacheName).then(function(cache) {
       return cache.match(event.request).then(function(response) {
         var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
+          // if we got a response from the cache, update the cache
+          if (response) {
+            console.log("cached page: " + event.request.url);
+            cache.put(event.request, networkResponse.clone());
+          }
           return networkResponse;
         });
         return response || fetchPromise;
