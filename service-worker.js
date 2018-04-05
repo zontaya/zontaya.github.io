f8143ef6e062b02247e700bf5b63ae3ef1c270f2
zontaya <zontaya@gmail.com>
2018-04-05 15:31:09 +0700

    -

--- service-worker.js
+++ service-worker.js
@@ -30,28 +30,19 @@ self.addEventListener("install", function(e) {
 });
 self.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches
-      .open(cacheName)
-      .then(function(cache) {
-        return cache.match(event.request).then(function(response) {
-          var fetchPromise = fetch(event.request)
-            .then(function(networkResponse) {
-              // if we got a response from the cache, update the cache
-              if (response) {
-                console.log("cached page: " + event.request.url);
-                cache.put(event.request, networkResponse.clone());
-              }
-              return networkResponse;
-            })
-            .catch(function(err) {
-              console.log("a:" + err);
-            });
-          return response || fetchPromise;
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          // if we got a response from the cache, update the cache
+          if (response) {
+            console.log("cached page: " + event.request.url);
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
         });
-      })
-      .catch(function(err) {
-        console.log("b:" + err);
-      })
+        return response || fetchPromise;
+      });
+    })
   );
 });
 
