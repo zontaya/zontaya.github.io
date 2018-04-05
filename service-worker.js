6306ad455f2df542b46d8adabc5484a881f0967d
zontaya <zontaya@gmail.com>
2018-04-05 13:44:53 +0700

    -

--- service-worker.js
+++ service-worker.js
@@ -31,7 +31,8 @@ self.addEventListener("install", function(e) {
 
 self.addEventListener("fetch", function(event) {
   var requestURL = new URL(event.request.url);
-  var freshResource = fetch(requestURL).then(function(response) {
+
+  var freshResource = fetch(requestURL.request).then(function(response) {
     var clonedResponse = response.clone();
     // Don't update the cache with error pages!
     if (response.ok) {
