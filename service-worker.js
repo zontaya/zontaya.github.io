237657abac4ea69658acf1891d7eb807927aa652
zontaya <zontaya@gmail.com>
2018-04-09 11:11:38 +0700

    -

--- service-worker.js
+++ service-worker.js
@@ -31,6 +31,7 @@ self.addEventListener("install", function(e) {
 });
 
 self.addEventListener("fetch", function(event) {
+  console.log("[ServiceWorker] fetch");
   event.respondWith(
     caches.open(cacheName).then(function(cache) {
       return cache.match(event.request).then(function(response) {
@@ -45,6 +46,7 @@ self.addEventListener("fetch", function(event) {
 });
 
 self.addEventListener("activate", function(event) {
+  console.log("[ServiceWorker] activate");
   event.waitUntil(
     caches.keys().then(function(c) {
       return Promise.all(
@@ -60,5 +62,56 @@ self.addEventListener("activate", function(event) {
           })
       );
     })
+  );
+});
+
+self.addEventListener("sync", function(event) {
+  console.log("[ServiceWorker] sync");
+  registration.showNotification("Sync event fired!");
+  if (event.id == "myFirstSync") {
+  }
+});
+
+self.addEventListener("push", function(event) {
+  console.log("Received a push message", event);
+
+  var title = "Yay a message.";
+  var body = "We have received a push message.";
+  var icon = "/images/icon-192x192.png";
+  var tag = "simple-push-demo-notification-tag";
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+  );
+});
+
+self.addEventListener("notificationclick", function(event) {
+  console.log("On notification click: ", event.notification.tag);
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window"
+      })
+      .then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url === "/" && "focus" in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow("/");
+        }
+      })
   );
 });
