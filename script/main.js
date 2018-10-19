6bf3085214970869d6e475527d5db93d78eafca9
sontaya chaisuwan <sontaya.c@somapait.com>
2018-10-19 12:01:41 +0700

    -

--- script/main.js
+++ script/main.js
@@ -1,21 +1,21 @@
 /*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
+ *
+ *  Push Notifications codelab
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
 
 /* eslint-env browser, es6 */
 /* eslint-disable no-unused-vars */
@@ -32,14 +32,14 @@ let swRegistration = null;
 if ("serviceWorker" in navigator && "PushManager" in window) {
   console.log("[ServiceWorker] and Push is supported");
   navigator.serviceWorker
-    .register("sw.js")
-    .then(function(swReg) {
+    .register("./sw.js")
+    .then(function (swReg) {
       swRegistration = swReg;
       swReg.update();
       console.log("[ServiceWorker] is registered", swReg);
       initializeUI();
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.error("[ServiceWorker] Error", error);
     });
 } else {
@@ -80,7 +80,7 @@ function updateBtn() {
 }
 
 function initializeUI() {
-  pushButton.addEventListener("click", function() {
+  pushButton.addEventListener("click", function () {
     pushButton.disabled = true;
     if (isSubscribed) {
       unsubscribeUser();
@@ -90,14 +90,14 @@ function initializeUI() {
   });
 
   // Set the initial subscription value
-  swRegistration.pushManager.getSubscription().then(function(subscription) {
+  swRegistration.pushManager.getSubscription().then(function (subscription) {
     isSubscribed = !(subscription === null);
     updateSubscriptionOnServer(subscription);
     if (isSubscribed) {
       console.log("User is subscribed.");
     } else {
       console.log("User is NOT subscribed.");
-      subscribeUser();
+
     }
     updateBtn();
   });
@@ -110,7 +110,7 @@ function subscribeUser() {
       userVisibleOnly: true,
       applicationServerKey: applicationServerKey
     })
-    .then(function(subscription) {
+    .then(function (subscription) {
       console.log("User is subscribed");
 
       updateSubscriptionOnServer(subscription);
@@ -119,7 +119,7 @@ function subscribeUser() {
 
       updateBtn();
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log("Failed to subscribe the user: ", err);
       updateBtn();
     });
@@ -138,6 +138,7 @@ function updateSubscriptionOnServer(subscription) {
     subscriptionDetails.classList.remove("is-invisible");
     console.log("User is subscribed:" + JSON.stringify(subscription));
   } else {
+    subscriptionJson.textContent = null;
     subscriptionDetails.classList.add("is-invisible");
   }
 }
@@ -145,15 +146,15 @@ function updateSubscriptionOnServer(subscription) {
 function unsubscribeUser() {
   swRegistration.pushManager
     .getSubscription()
-    .then(function(subscription) {
+    .then(function (subscription) {
       if (subscription) {
         return subscription.unsubscribe();
       }
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log("Error unsubscribing", error);
     })
-    .then(function() {
+    .then(function () {
       updateSubscriptionOnServer(null);
       console.log("User is unsubscribed.");
       isSubscribed = false;
